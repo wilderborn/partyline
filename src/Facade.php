ca1f340906637a552a237b7fbbb9fe0688289cef
Johnathan Douglas de Souza Santos <johdougss@gmail.com>
2019-09-11 17:53:16 -0300

    better type hinting
    add type hinting Wilderborn\Partyline\Facade

--- src/Facade.php
+++ src/Facade.php
@@ -4,6 +4,14 @@ namespace Wilderborn\Partyline;
 
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
+/**
+ * Class Facade
+ * @method static void bind(Command $console)
+ * @method static \Symfony\Component\Console\Output\OutputInterface getOutput()
+ * @method static void info(string $string, int|string|null $verbosity = null)
+ *
+ * @see \Wilderborn\Partyline\Partyline
+ */
 class Facade extends IlluminateFacade
 {
     protected static function getFacadeAccessor()
