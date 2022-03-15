32623756e376da900e4554f3150437527c88a2ef
Johnathan Douglas de Souza Santos <johdougss@gmail.com>
2022-03-15 17:23:29 -0300

    Add Facade hints (#4)

--- src/Facade.php
+++ src/Facade.php
@@ -4,6 +4,21 @@ namespace Wilderborn\Partyline;
 
 use Illuminate\Support\Facades\Facade as IlluminateFacade;
 
+/**
+ * @method static void bind(\Illuminate\Console\Command $console)
+ * @method static \Symfony\Component\Console\Output\OutputInterface getOutput()
+ * @method static void line(string $string, string $style = null, int|string|null $verbosity = null)
+ * @method static void info(string $string, int|string|null $verbosity = null)
+ * @method static void comment(string $string, int|string|null $verbosity = null)
+ * @method static void question(string $string, int|string|null $verbosity = null)
+ * @method static void error(string $string, int|string|null $verbosity = null)
+ * @method static void warn(string $string, int|string|null $verbosity = null)
+ * @method static void alert(string $string)
+ * @method static void table(array $headers, \Illuminate\Contracts\Support\Arrayable|array $rows, string $tableStyle = 'default', array $columnStyles = [])
+ *
+ * @see \Wilderborn\Partyline\Partyline
+ * @see \Illuminate\Console\Command
+ */
 class Facade extends IlluminateFacade
 {
     protected static function getFacadeAccessor()
