40a43f61a874512c7d94007b89dc6e4d8cbabb13
Roberto Rielo <roberto910907@gmail.com>
2022-11-24 17:24:19 -0500

    Added missing methods to interact with the command line

--- src/Facade.php
+++ src/Facade.php
@@ -8,6 +8,12 @@ use Illuminate\Support\Facades\Facade as IlluminateFacade;
  * @method static void bind(\Illuminate\Console\Command $console)
  * @method static \Symfony\Component\Console\Output\OutputInterface getOutput()
  * @method static void line(string $string, string $style = null, int|string|null $verbosity = null)
+ * @method static bool confirm(string $question, bool $default = false)
+ * @method static mixed ask(string $question, string|null $default = null)
+ * @method static mixed anticipate(string $question, array|callable $choices, string|null $default = null)
+ * @method static mixed askWithCompletion(string $question, array|callable $choices, string|null $default = null)
+ * @method static mixed secret(string $question, bool $fallback = true)
+ * @method static string|array choice(string $question, array $choices, string|int|null $default = null, mixed|null $attempts = null, bool $multiple = false)
  * @method static void info(string $string, int|string|null $verbosity = null)
  * @method static void comment(string $string, int|string|null $verbosity = null)
  * @method static void question(string $string, int|string|null $verbosity = null)
@@ -15,7 +21,7 @@ use Illuminate\Support\Facades\Facade as IlluminateFacade;
  * @method static void warn(string $string, int|string|null $verbosity = null)
  * @method static void alert(string $string)
  * @method static void newLine(int $count = 1)
- * @method static void table(array $headers, \Illuminate\Contracts\Support\Arrayable|array $rows, string $tableStyle = 'default', array $columnStyles = [])
+ * @method static void table(array $headers, \Illuminate\Contracts\Support\Arrayable|array $rows, \Symfony\Component\Console\Helper\TableStyle|string $tableStyle = 'default', array $columnStyles = [])
  *
  * @see \Wilderborn\Partyline\Partyline
  * @see \Illuminate\Console\Concerns\InteractsWithIO
