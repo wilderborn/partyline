74dcc591b5581c24e8be848e45ef5f59d8376c86
Roberto Rielo <roberto910907@gmail.com>
2022-12-14 13:38:34 -0500

    Added missing Facade phpdocs (#9)
    Co-authored-by: Jason Varga <jason@pixelfear.com>
    Closes https://github.com/wilderborn/partyline/issues/8

--- src/Facade.php
+++ src/Facade.php
@@ -8,15 +8,23 @@ use Illuminate\Support\Facades\Facade as IlluminateFacade;
  * @method static void bind(\Illuminate\Console\Command $console)
  * @method static \Symfony\Component\Console\Output\OutputInterface getOutput()
  * @method static void line(string $string, string $style = null, int|string|null $verbosity = null)
+ * @method static bool confirm(string $question, bool $default = false)
+ * @method static mixed ask(string $question, string|null $default = null)
+ * @method static mixed anticipate(string $question, array|callable $choices, string|null $default = null)
+ * @method static mixed askWithCompletion(string $question, array|callable $choices, string|null $default = null)
+ * @method static mixed secret(string $question, bool $fallback = true)
+ * @method static string|array choice(string $question, array $choices, string|int|null $default = null, mixed|null $attempts = null, bool $multiple = false)
  * @method static void info(string $string, int|string|null $verbosity = null)
  * @method static void comment(string $string, int|string|null $verbosity = null)
  * @method static void question(string $string, int|string|null $verbosity = null)
  * @method static void error(string $string, int|string|null $verbosity = null)
  * @method static void warn(string $string, int|string|null $verbosity = null)
  * @method static void alert(string $string)
- * @method static void table(array $headers, \Illuminate\Contracts\Support\Arrayable|array $rows, string $tableStyle = 'default', array $columnStyles = [])
+ * @method static void newLine(int $count = 1)
+ * @method static void table(array $headers, \Illuminate\Contracts\Support\Arrayable|array $rows, \Symfony\Component\Console\Helper\TableStyle|string $tableStyle = 'default', array $columnStyles = [])
  *
  * @see \Wilderborn\Partyline\Partyline
+ * @see \Illuminate\Console\Concerns\InteractsWithIO
  * @see \Illuminate\Console\Command
  */
 class Facade extends IlluminateFacade
